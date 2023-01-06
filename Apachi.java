f7ad18fad1d0a854928f49b72b399bbbb140938d
niarjkadbhane <122081321+niarjkadbhane@users.noreply.github.com>
2023-01-06 13:08:37 +0530

    Add files via upload

---
+++ Apachi.java
@@ -0,0 +1,12 @@
+class Apachi extends Bike
+{
+public void mileage()
+{
+System.out.println("mileage=45");
+System.out.println("I"+(char)3+"Apachi");
+}
+public static void main(String[]args)
+{
+new Apachi().mileage();
+}
+}
